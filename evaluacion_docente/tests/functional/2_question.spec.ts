bb1f9b8a6459689429c14413b1e588c36a441475
Cristian Cubillos <ccubillosreyes123@gmail.com>
2023-03-15 21:18:59 -0500

    Minor code refactors

--- evaluacion_docente/tests/functional/2_question.spec.ts
+++ evaluacion_docente/tests/functional/2_question.spec.ts
@@ -3,38 +3,6 @@ import { test } from '@japa/runner'
 import { getToken } from './getToken'
 
 test.group('Question CRUD tests...',()=>{
-    // test('Should store a form without questions...',async ({ client }) => {
-    //     const token = await getToken(1) // Obtener token de un usuario con rol 'admin'
-    //     const response = await client.post('api/v1/form').header('Authorization',`Bearer ${token}`).json({
-    //         "formName":"mockForm"
-    //     }
-    //     )
-    //     response.assertStatus(200)
-    //     response.body().assert?.isObject()
-    //     response.assert?.containsSubset(response.body(),{"message":"Formulario creado exitosamente"})
-    // })
-
-    // test('Should store a question and its options when valid data is given...',async ({ client }) => {
-    //     const token = await getToken(1) // Obtener token de un usuario con rol 'admin'
-    //     await client.post('api/v1/questions/create').header('Authorization',`Bearer ${token}`).json({
-    //         "formName":"mockFormWithQuestions"
-    //     }).then(async () => {
-    //         const response = await client.post('api/v1/question').header('Authorization',`Bearer ${token}`).json({
-    //             "formId":2,
-    //             "question": "¿que dia es hoy?",
-    //             "options": [
-    //                 'lunes',
-    //                 'martes',
-    //                 'miercoles',
-    //                 'jueves'
-    //             ]
-    //         }
-    //         )
-    //         response.assertStatus(200)
-    //         response.body().assert?.isObject()
-    //         response.assert?.containsSubset(response.body(),{"message":"Pregunta creada exitosamente"})
-    //     })
-    // })
 
     test('Should store a question and its options when valid data is given...',async ({ client }) => {
         const token = await getToken(1) // Obtener token de un usuario con rol 'admin'
@@ -129,20 +97,4 @@ test.group('Question CRUD tests...',()=>{
             assert.properties(err,['state','message','error'])
         }
     })
-
-
-
-    // test('Should list all forms ids and names...',async ({ client }) => {
-    //     const response = await client.get('api/v1/form')
-    //     response.assertStatus(200)
-    //     response.assert?.isArray(response.body())
-    // })
-
-    // test('Should list form questions and optiones when giving a valid id',async ({ client }) => {
-    //     const response = await client.get('api/v1/form/2')
-    //     response.assertStatus(200)
-    //     response.assert?.isObject(response.body())
-    // })
-
-    
 })
